4c2a1feb43687bd922e2c50d147c0c8c545dff2d
Donald Le <ledinhcuong99@gmail.com>
2022-12-02 20:54:56 +0700

    Using pytest instead of unit test

--- user_app/tests/test_main.py
+++ user_app/tests/test_main.py
@@ -9,40 +9,43 @@ from user_app.main import app
 client = TestClient(app)
 
 
-class TestUser(unittest.TestCase):
-    def test_create_user(self):
-        username = ''.join(random.choice(string.ascii_letters) for i in range(10))
-        response = client.post("/user", json={
-            "username": username,
-            "password": "12345",
-            "fullname": "Medium aloha"
-        })
-        assert response.status_code == 200
-        assert response.json()["username"] == username
+def test_create_user():
+    username = ''.join(random.choice(string.ascii_letters) for i in range(10))
+    response = client.post("/user", json={
+        "username": username,
+        "password": "12345",
+        "fullname": "Medium aloha"
+    })
+    assert response.status_code == 200
+    assert response.json()["username"] == username
 
-    def test_authenticate_user_success(self):
-        response = client.post("/authenticate", json={
-            "username": "medium",
-            "password": "12345",
-        })
-        assert response.status_code == 200
 
-    def test_authenticate_user_wrong_password(self):
-        response = client.post("/authenticate", json={
-            "username": "medium",
-            "password": "123456",
-        })
-        assert response.status_code == 400
-        assert response.json()["detail"] == "Password is not correct"
+def test_authenticate_user_success():
+    response = client.post("/authenticate", json={
+        "username": "medium",
+        "password": "12345",
+    })
+    assert response.status_code == 200
 
-    def test_authenticate_user_wrong_username(self):
-        response = client.post("/authenticate", json={
-            "username": "medium55",
-            "password": "123456",
-        })
-        assert response.status_code == 400
-        assert response.json()["detail"] == "Username not existed"
+
+def test_authenticate_user_wrong_password():
+    response = client.post("/authenticate", json={
+        "username": "medium",
+        "password": "123456",
+    })
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Password is not correct"
+
+
+def test_authenticate_user_wrong_username():
+    response = client.post("/authenticate", json={
+        "username": "medium55",
+        "password": "123456",
+    })
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Username not existed"
 
 
 if __name__ == "__main__":
-    unittest.main()
+    import pytest
+    raise SystemExit(pytest.main([__file__]))
